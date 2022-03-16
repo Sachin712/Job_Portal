4ba29a320f6822db6a39eb4629cc7ef46e0c772d
Sachin Devdhar <sachdevdhar@gmail.com>
2022-03-15 22:15:49 -0400

    completed video 23

--- app/src/main/java/com/android_project/job_portal/HomeActivity.java
+++ app/src/main/java/com/android_project/job_portal/HomeActivity.java
@@ -1,6 +1,7 @@
 package com.android_project.job_portal;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,6 +13,9 @@ public class HomeActivity extends AppCompatActivity {
     private Button btnAllJob;
     private Button btnPostJob;
 
+    //Toolbar
+    private Toolbar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +24,10 @@ public class HomeActivity extends AppCompatActivity {
 
         btnAllJob = findViewById(R.id.btn_alljob);
         btnPostJob = findViewById(R.id.btn_postjob);
+        toolbar = findViewById(R.id.toolbar_home);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setTitle("Job Portal App");
 
         btnAllJob.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/android_project/job_portal/PostJobsActivity.java
+++ app/src/main/java/com/android_project/job_portal/PostJobsActivity.java
@@ -2,13 +2,29 @@ package com.android_project.job_portal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class PostJobsActivity extends AppCompatActivity {
+
+    private FloatingActionButton fabBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_jobs);
+
+        fabBtn = findViewById(R.id.fab_add);
+
+        fabBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), insert_JobPostActivity.class));
+            }
+        });
+
     }
 }
